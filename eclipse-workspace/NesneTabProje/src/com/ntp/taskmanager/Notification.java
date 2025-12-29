d0d402eb451842859179a636cea8923698d5ed22
anillemirr <anilemirok@gmail.com>
2025-12-29 23:47:14 +0300

    Delete eclipse-workspace/NesneTabProje directory

--- eclipse-workspace/NesneTabProje/src/com/ntp/taskmanager/Notification.java
+++
@@ -1,9 +0,0 @@
-package com.ntp.taskmanager;
-
-public class Notification {
-    public static String upcoming(Task task) {
-        return "⏰ Yaklaşan görev: " + task.getTitle()
-                + " | Deadline: " + task.getDeadline().getDue()
-                + " | Öncelik: " + task.getPriority();
-    }
-}
